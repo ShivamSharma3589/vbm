c75410e125658487d0ef375d396c010bc8b78c4f
Shivam Sharma <shivam.sharma2@velsof.com>
2023-05-13 08:31:41 +0530

    Added the Link to the Navigation bars

--- src/components/bottom-nav/BottomNav.js
+++ src/components/bottom-nav/BottomNav.js
@@ -9,10 +9,15 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import "./BottomNav.css";
+import { useNavigate } from "react-router-dom";
 
 const BottomNav = () => {
-  //#region
+  //#region for state variables
   const [value, setValue] = React.useState(0);
+  //#endregion
+
+  //#region for Objects and Function
+  const navigate = useNavigate();
   //#endregion
 
   return (
@@ -24,10 +29,18 @@ const BottomNav = () => {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="Home"
+          icon={<HomeIcon />}
+          onClick={() => navigate("/")}
+        />
         <BottomNavigationAction label="Category" icon={<CategoryIcon />} />
         <BottomNavigationAction label="Cart" icon={<ShoppingCartIcon />} />
-        <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />} />
+        <BottomNavigationAction
+          label="Profile"
+          icon={<AccountCircleIcon />}
+          onClick={() => navigate("/about")}
+        />
       </BottomNavigation>
     </div>
   );
---
+++ src/pages/about-us-page/AboutUsPage.js
@@ -0,0 +1,12 @@
+/**
+ * ABOUT US PAGE
+ * @author: shivayasharma1149@gmail.com
+ * @date : 13 May 2023
+ */
+import React from "react";
+
+const AboutUsPage = () => {
+  return <div>AboutUsPage</div>;
+};
+
+export default AboutUsPage;
