1731cb84fd79de4827126e67d2603243a3ef6a10
Shivam Sharma <shivam.sharma2@velsof.com>
2023-05-13 07:56:58 +0530

    Changed the banner component's folder name

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Banner from "./carousel/Banner";
 import BottomNav from "./bottom-nav/BottomNav";
 import ProductGrid from "./product-grid/ProductGrid";
 import Header from "./header/Header";
-import AppBannerCarousel from "./app-carousel/AppBannerCarousel";
+import AppBannerCarousel from "./app-banner-carousel/AppBannerCarousel";
 import AppCategoryCarousel from "./app-category-carousel/AppCategoryCarousel";
 import AppProductsGrid from "./app-products-grid/AppProductsGrid";
 import AppProductCarousel from "./app-product-carousel/AppProductCarousel";
